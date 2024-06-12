ddc91f7ffd88d0f65f5c152aa6752733249be4a8
Emre Can Çorapçı <emrecancorapci@gmail.com>
2024-06-13 01:26:11 +0300

    Add swr and autofixer packages, fetcher function for swr and components.json for shadcn/ui

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
-import { type ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+// @ts-expect-error swr
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+export { fetcher };
